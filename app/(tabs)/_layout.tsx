9a288cacbd85a454bfd4f6eb51cd3600fb0f72ff
Ali Hassan <hassankasuri0@gmail.com>
2025-07-05 18:13:20 +0500

    Removed unused files

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -35,13 +35,6 @@ export default function TabLayout() {
           headerShown: true,
         }}
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
     </Tabs>
   );
 }
